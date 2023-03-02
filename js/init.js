c24f7b86b0678431cd004e5bffb5bd5b7a7cee71
Grupo-13-MT <114522642+Grupo-13-MT@users.noreply.github.com>
2023-03-01 22:58:54 -0500

    Se integran graficos estaticos en mis rendimientos

---
+++ js/init.js
@@ -0,0 +1,25 @@
+const url = "https://cooper-production.up.railway.app/";
+
+function graficar(ctxid, labelp, labelsp, datap) {
+    let ctx = document.getElementById(ctxid).getContext('2d');
+    var obj = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labelsp,
+            datasets: [{
+                label: labelp,
+                data: datap,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
